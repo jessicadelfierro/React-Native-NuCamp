b9951bf8d9421a2b6cad85f4cd2bbbf625d76c1f
Jessica Del Fierro <jessicabaldric@gmail.com>
2022-05-12 22:37:56 -0400

    Components part 1

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Main from './components/MainComponent';
+
+
+//view: generic container component that supports layout with flexbox styles and more (like html div element)
+//text: generic container for displaying text
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
