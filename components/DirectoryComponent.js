8c512d84a4e796ec0727f44f17a7b442650f1db7
Jessica Del Fierro <jessicabaldric@gmail.com>
2022-05-14 07:30:19 -0400

    navigation part 1

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-function Directory(props) {
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
 
-    const renderDirectoryItem = ({item}) => {
-        return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
-            />
-        )
+    //configure title text for the header
+    //static is a javascript keyword that sets a method on the class itself rather than the object that is instantiated from the class
+    //it's being used by react native to communicate this title to the directory navigator's navigation option
+
+    static navigationOptions = {
+        title: 'Directory'
     };
 
-    return (
-        <FlatList
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-        />
-    );
+    render() {
+
+        const { navigate } = this.props.navigation; 
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
+                    // first argument is the name of the screen to navigate to
+                    // second argument adds extra parameters to the route, specifying a parameter called 'campsiteId' and giving it the id of the campsite that was passed
+                    //when an item in the directory is pressed, it will call the navigate function from react navigation in order to switch to the campsiteInfo screen.  the campsite id parameter will be used to pass the correct campsite object to it
+                    leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                />
+            )
+        };
+    
+        return (
+            <FlatList
+                data={this.state.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        );
+    }
 }
 
 export default Directory;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';
+import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+
+//createStackNavigator is a function, that has 1 required argument called "Route Config Object"
+// in this argument, we set what components will be available for this stack
+// the second argument is optional, initialRouteName -- default to showing said component
+// defaultNavigationOptions is where you'd configure the settings for the header
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+//stack navigator needs to be passed to the function createAppContainer
+//createAppContainer will return a react component that handles connecting our top level navigator to the react native application environment to handle some functions such as responding to the back button on a device
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
+
+//Main component will act as the central hub that creates and holds all of the navigators
+// (AppNavigator is a container for the DirectoryNavigator which contains the screens for both directory and campsiteinfo)
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
     render() {
         return (
-            <View style={{flex: 1}}>
-                <Directory 
-                    campsites={this.state.campsites}
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)} 
-                />
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} 
-                />
+            <View 
+                style={{
+                        flex: 1,
+                        paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+                        //different paddingTop depending on the operating system
+                        //Constants.statusBarHeight sets the dynamic status bar height
+                        //Platform API is used to get the operating system of the device,
+                    }}
+            >
+                <AppNavigator />
             </View>
         );
     }
